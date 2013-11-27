55c45733d18ced5f339e0248e243780e343f03c0
Thomas GERVAIS <thomas.gervais@st.com>
2013-11-27 17:56:42 +0100

    Added unit tests for Configuration & Plugin

---
+++ UnitTests/Configuration/IniConfigurationTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PA.Configuration;
+
+namespace UnitTests.Configuration
+{
+    [TestClass]
+    public class IniConfigurationTest
+    {
+      
+        [TestMethod]
+        public void ContainsSetting()
+        {
+            IniConfigurationSource src = new IniConfigurationSource();
+
+            Assert.IsTrue(src.ContainsSetting("TestSection/TestString"));
+            Assert.IsFalse(src.ContainsSetting("TestSection/CommentedItem"));
+        }
+
+        [TestMethod]
+        public void Read()
+        {
+            IniConfigurationSource src = new IniConfigurationSource();
+
+            Assert.AreEqual("Correct", src.GetSetting("TestSection/TestString"));
+        }
+
+        [TestMethod]
+        public void Write()
+        {
+            IniConfigurationSource src = new IniConfigurationSource();
+
+            string testString = DateTime.Now.Ticks.ToString();
+
+            src.SetSetting("TestSection/Now", testString);
+
+            Assert.AreEqual(testString, src.GetSetting("TestSection/Now"));
+        }
+    }
+}
