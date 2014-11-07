78b2172f26fb18afd1b74ee036caf5625f0607eb
Thomas GERVAIS <thomas.gervais@st.com>
2014-11-07 09:29:14 +0100

    Added Crop

--- PA.TileList.Geometrics/Extensions/CoordinateExtensions.cs
+++ PA.TileList.Geometrics/Extensions/CoordinateExtensions.cs
@@ -1,4 +1,8 @@
-﻿using PA.TileList.Geometrics.Line;
+﻿
+using PA.TileList.Contextual;
+using PA.TileList.Extensions;
+using PA.TileList.Quantified;
+using PA.TileList.Geometrics.Line;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -54,7 +58,7 @@ namespace PA.TileList.Geometrics
                 && p.Y >= Math.Min(l.Coordinate1.Y, l.Coordinate2.Y);
         }
 
-        public static bool IsInside<T>(this T p,  T[] polygon)
+        public static bool IsInside<T>(this T p, T[] polygon)
              where T : ICoordinate
         {
             // There must be at least 3 vertices in polygon[]
@@ -94,5 +98,131 @@ namespace PA.TileList.Geometrics
             return count % 2 == 1;
         }
 
+        #region Translate
+
+        public enum TranslateSource
+        {
+            Center,
+            Min, Max
+        }
+
+        /// <summary>
+        /// Translate all list so that specified source match coordinate [0,0] 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="c"></param>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static IEnumerable<IContextual<T>> Translate<T>(this IEnumerable<T> c, TranslateSource source)
+            where T : ICoordinate
+        {
+            switch (source)
+            {
+                case TranslateSource.Min:
+                    return c.Translate(c.GetArea().Min, Coordinate.Zero);
+                case TranslateSource.Max:
+                    return c.Translate(c.GetArea().Max, Coordinate.Zero);
+                default:
+                    return c.Translate(c.GetArea().Center(), Coordinate.Zero);
+            }
+        }
+
+        /// <summary>
+        /// Translate all list so that specified source match coordinate [0,0] 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="c"></param>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static IEnumerable<IContextual<T>> Translate<T>(this IEnumerable<IContextual<T>> c, TranslateSource source)
+            where T : ICoordinate
+        {
+            switch (source)
+            {
+                case TranslateSource.Min:
+                    return c.Translate(c.GetArea().Min, Coordinate.Zero);
+                case TranslateSource.Max:
+                    return c.Translate(c.GetArea().Max, Coordinate.Zero);
+                default:
+                    return c.Translate(c.GetArea().Center(), Coordinate.Zero);
+            }
+        }
+
+        public static ITile<IContextual<T>> Translate<T>(this ITile<T> t, TranslateSource source)
+            where T : ICoordinate
+        {
+            return CoordinateExtensions.Translate<T>(t.AsEnumerable(), source)
+                .AsTile(t.IndexOf(t.Reference))
+                .RefreshArea();
+        }
+
+        public static ITile<IContextual<T>> Translate<T>(this ITile<IContextual<T>> t, TranslateSource source)
+           where T : ICoordinate
+        {
+            return CoordinateExtensions.Translate<T>(t.AsEnumerable(), source)
+                .AsTile(t.IndexOf(t.Reference))
+                .RefreshArea();
+        }
+
+        public static IQuantifiedTile<IContextual<T>> Translate<T>(this IQuantifiedTile<T> t, TranslateSource source)
+            where T : ICoordinate
+        {
+            return CoordinateExtensions.Translate<T>(t.AsTile(), source)
+                .AsQuantified(t.ElementSizeX, t.ElementSizeY, t.ElementStepX, t.ElementStepY, t.RefOffsetX, t.RefOffsetY);
+        }
+
+        public static IQuantifiedTile<IContextual<T>> Translate<T>(this IQuantifiedTile<IContextual<T>> t, TranslateSource source)
+           where T : ICoordinate
+        {
+            return CoordinateExtensions.Translate<T>(t.AsTile(), source)
+                .AsQuantified(t.ElementSizeX, t.ElementSizeY, t.ElementStepX, t.ElementStepY, t.RefOffsetX, t.RefOffsetY);
+        }
+
+
+        /// <summary>
+        /// Translate all list so that source match destination
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="c"></param>
+        /// <param name="refOrigin"></param>
+        /// <param name="refDest"></param>
+        /// <returns></returns>
+        public static IEnumerable<IContextual<T>> Translate<T>(this IEnumerable<T> c, ICoordinate refOrigin, ICoordinate refDest)
+            where T : ICoordinate
+        {
+            int offsetX = refDest.X - refOrigin.X;
+            int offsetY = refDest.Y - refOrigin.Y;
+
+            foreach (T e in c)
+            {
+                yield return new Contextual<T>(e.X + offsetX, e.Y + offsetY, e);
+            }
+        }
+
+
+        /// <summary>
+        /// Translate all list so that source match destination
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="c"></param>
+        /// <param name="refOrigin"></param>
+        /// <param name="refDest"></param>
+        /// <returns></returns>
+        public static IEnumerable<IContextual<T>> Translate<T>(this IEnumerable<IContextual<T>> c, ICoordinate refOrigin, ICoordinate refDest)
+            where T : ICoordinate
+        {
+            int offsetX = refDest.X - refOrigin.X;
+            int offsetY = refDest.Y - refOrigin.Y;
+
+            foreach (IContextual<T> e in c)
+            {
+                e.X += offsetX;
+                e.Y += offsetY;
+            }
+
+            return c;
+        }
+
+        #endregion
     }
 }
