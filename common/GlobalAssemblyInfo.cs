6ba292bec445ee432f3deeaa91a713cefb1dbad3
Thomas GERVAIS <thomas.gervais@st.com>
2014-12-16 14:10:17 +0100

    Integrate Package Generation

--- common/GlobalAssemblyInfo.cs
+++ common/GlobalAssemblyInfo.cs
@@ -1,6 +1,5 @@
 ﻿using System.Reflection;
 
-
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
 #else
@@ -12,14 +11,3 @@
 [assembly: AssemblyCopyright("©PA 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("4.0.*")]
