557e3528a5ce891521eb3fab79be08dc71f57079
Thomas GERVAIS <thomas.gervais@st.com>
2015-03-11 11:51:05 +0100

    Add PascalCase & CamelCase Support

--- PA.Converters/Extensions/StringExtensions.cs
+++ PA.Converters/Extensions/StringExtensions.cs
@@ -35,5 +35,44 @@ namespace PA.Converters.Extensions
 
             throw new InvalidCastException("Not an array");
         }
+
+        public static string ToPascalCase(this string str)
+        {
+            // If there are 0 or 1 characters, just return the string.
+            if (str == null)
+                return str;
+            if (str.Length < 2)
+                return str.ToUpper();
+
+            // Split the string into words.
+            string[] words = str.Split(new char[] { }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Combine the words.
+            string result = "";
+            foreach (string word in words)
+            {
+                result += word.Substring(0, 1).ToUpper() + word.Substring(1);
+            }
+            return result;
+        }
+
+        public static string ToCamelCase(this string str)
+        {
+            // If there are 0 or 1 characters, just return the string.
+            if (str == null || str.Length < 2)
+                return str;
+
+            // Split the string into words.
+            string[] words = str.Split(new char[] { }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Combine the words.
+            string result = words[0].ToLower();
+            for (int i = 1; i < words.Length; i++)
+            {
+                result += words[i].Substring(0, 1).ToUpper() + words[i].Substring(1);
+            }
+
+            return result;
+        }
     }
 }
