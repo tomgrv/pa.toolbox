7a80aacbcd9b90e06ba6f104b14f0b86bc171b96
Thomas GERVAIS <thomas.gervais@st.com>
2015-08-17 17:13:04 +0200

    [Upd] Moved test files

---
+++ Tests/NTests/Objects/IPluginUri.cs
@@ -0,0 +1,14 @@
+﻿using PA.Plugin;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+
+namespace PA.Plugin.Tests.Objects
+{
+    public interface IPluginUri : IPlugin<Uri>
+    {
+   
+    }
+}
---
+++ Tests/NTests/Objects/PluginForSpecificArrayTest.cs
@@ -0,0 +1,29 @@
+﻿using PA.Plugin;
+using PA.Plugin.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+
+namespace PA.Plugin.Tests.Objects
+{
+    [Export]
+    [PartCreationPolicy(System.ComponentModel.Composition.CreationPolicy.NonShared)]
+    public class PluginForSpecificArrayTest : IPlugin
+    {
+        [Import("#/Param")]
+        public string Parameter { get; set; }
+
+        public PluginForSpecificArrayTest()
+        {
+            
+        }
+
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
