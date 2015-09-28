f84a9d60c546cf94018688ea0b1d34a5edd83cb1
Thomas GERVAIS <thomas.gervais@st.com>
2015-09-28 11:50:18 +0200

    [Upd] Changed Date

--- common/GlobalAssemblyInfo.cs
+++ common/GlobalAssemblyInfo.cs
@@ -8,6 +8,6 @@
 
 [assembly: AssemblyCompany("PerspicApps")]
 [assembly: AssemblyProduct("PA.Toolbox")]
-[assembly: AssemblyCopyright("©PA 2014")]
+[assembly: AssemblyCopyright("©PA 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
