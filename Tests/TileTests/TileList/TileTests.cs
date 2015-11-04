6e5b1bbaf2e12acfe48bec6a09c5a34dcddcbc88
Thomas GERVAIS <thomas.gervais@st.com>
2015-11-04 11:21:26 +0100

    [Add] Fullspace Tile for tests

--- Tests/TileTests/TileList/TileTests.cs
+++ Tests/TileTests/TileList/TileTests.cs
@@ -11,144 +11,170 @@ using NUnit.Framework;
 
 namespace PA.TileList
 {
-    [TestFixtureAttribute]
-    public class TileTests
-    {
-        #region Definitions 
+	[TestFixtureAttribute]
+	public class TileTests
+	{
+		#region Definitions
 
-        internal class MainTile : Tile<SubTile>, IQuadrant<SubTile>, IQuantifiedTile<SubTile>, ITile<SubTile>
-        {
-            public MainTile(IArea a, SubTile t)
-                : base(a, t)
+		internal class MainTile : Tile<SubTile>, IQuadrant<SubTile>, IQuantifiedTile<SubTile>, ITile<SubTile>
+		{
+			public MainTile(IArea a, SubTile t)
+				: base(a, t)
             { }
 
-            public Quadrant.Quadrant Quadrant { get; private set; }
+			public Quadrant.Quadrant Quadrant { get; private set; }
 
-            public void SetQuadrant(Quadrant.Quadrant q)
-            {
-                throw new NotImplementedException();
-            }
+			public void SetQuadrant(Quadrant.Quadrant q)
+			{
+				throw new NotImplementedException();
+			}
 
-            public double ElementSizeX { get; internal set; }
+			public double ElementSizeX { get; internal set; }
 
-            public double ElementSizeY { get; internal set; }
+			public double ElementSizeY { get; internal set; }
 
-            public double ElementStepX { get; internal set; }
+			public double ElementStepX { get; internal set; }
 
-            public double ElementStepY { get; internal set; }
+			public double ElementStepY { get; internal set; }
 
-            public double RefOffsetX { get; internal set; }
+			public double RefOffsetX { get; internal set; }
 
-            public double RefOffsetY { get; internal set; }
+			public double RefOffsetY { get; internal set; }
 
           
-        }
+		}
 
-        internal class SubTile : Tile<Item>, IQuadrant<Item>
-        {
+		internal class SubTile : Tile<Item>, IQuadrant<Item>
+		{
            
-            public SubTile(IArea a, Item t)
-                : base(a, t)
+			public SubTile(IArea a, Item t)
+				: base(a, t)
             { }
 
-            public SubTile(SubTile t)
-                : base(t)
+			public SubTile(SubTile t)
+				: base(t)
             { }
 
-            public SubTile(IEnumerable<Item> t, int referenceIndex = 0)
-                : base(t, referenceIndex)
+			public SubTile(IEnumerable<Item> t, int referenceIndex = 0)
+				: base(t, referenceIndex)
             { }
 
-            public Quadrant.Quadrant Quadrant { get; private set; }
+			public Quadrant.Quadrant Quadrant { get; private set; }
 
-            public void SetQuadrant(Quadrant.Quadrant q)
-            {
-                throw new NotImplementedException();
-            }
+			public void SetQuadrant(Quadrant.Quadrant q)
+			{
+				throw new NotImplementedException();
+			}
 
-        }
+		}
 
-        internal class Item : Coordinate
-        {
-            public Color Color { get; set; }
+		internal class Item : Coordinate
+		{
+			public Color Color { get; set; }
 
-            public Item(int x, int y, Color c)
-                : base(x, y)
-            {
-                this.Color = c;
-            }
+			public Item(int x, int y, Color c)
+				: base(x, y)
+			{
+				this.Color = c;
+			}
 
-            public Bitmap ToBitmap(int w, int h, string s)
-            {
-                Bitmap b = new Bitmap(w, h);
+			public Bitmap ToBitmap(int w, int h, string s)
+			{
+				Bitmap b = new Bitmap(w, h);
                 
-                using (Graphics g = Graphics.FromImage(b))
-                {
-                    g.DrawRectangle(Pens.Pink, 0, 0, w - 1, h - 1);
-                    g.FillRectangle(new SolidBrush(this.Color), 1, 1, w - 2, h - 2);
-                    g.DrawString(s, new Font(FontFamily.GenericSansSerif, (float)w / 3f), Brushes.Gray, 0, 0);
-                }
+				using (Graphics g = Graphics.FromImage(b))
+				{
+					g.DrawRectangle(Pens.Pink, 0, 0, w - 1, h - 1);
+					g.FillRectangle(new SolidBrush(this.Color), 1, 1, w - 2, h - 2);
+					g.DrawString(s, new Font(FontFamily.GenericSansSerif, (float)w / 3f), Brushes.Gray, 0, 0);
+				}
 
-                return b;
-            }
+				return b;
+			}
 
-        }
+		}
 
-        internal static MainTile GetTile(float factor)
-        {
-            IArea first = new Area((int)(-5 * factor), (int)(-5 * factor), (int)(5 * factor), (int)(5 * factor));
-            IArea second = new Area(1, 1, 5, 5);
+		internal static MainTile GetTile(float factor)
+		{
+			IArea first = new Area((int)(-5 * factor), (int)(-5 * factor), (int)(5 * factor), (int)(5 * factor));
+			IArea second = new Area(1, 1, 5, 5);
             
-            SubTile t1 = new SubTile(second, new Item(3, 3, Color.Green));
-            t1.Fill(c => new Item(c.X, c.Y,  c.X + c.Y == 6 ? Color.Green : Color.Yellow));
+			SubTile t1 = new SubTile(second, new Item(3, 3, Color.Green));
+			t1.Fill(c => new Item(c.X, c.Y, c.X + c.Y == 6 ? Color.Green : Color.Yellow));
 
-            MainTile t0 = new MainTile(first, t1);
+			MainTile t0 = new MainTile(first, t1);
             t0.Fill(c => t1.Clone(c.X,c.Y) as SubTile);
 
-            t0.ElementSizeX = 50f / factor * second.SizeX;
-            t0.ElementSizeY = 50f / factor * second.SizeY;
-            t0.ElementStepX = 55f / factor * second.SizeX;
-            t0.ElementStepY = 60f / factor * second.SizeY;
-            t0.RefOffsetX = 50;
-            t0.RefOffsetY = 0;
+			t0.ElementSizeX = 50f / factor * second.SizeX;
+			t0.ElementSizeY = 50f / factor * second.SizeY;
+			t0.ElementStepX = 55f / factor * second.SizeX;
+			t0.ElementStepY = 60f / factor * second.SizeY;
+			t0.RefOffsetX = 50;
+			t0.RefOffsetY = 0;
 
-            return t0;
-        }
+			return t0;
+		}
 
-        #endregion
+		internal static MainTile GetTileFullSpace(float factor)
+		{
+			IArea first = new Area((int)(-5 * factor), (int)(-5 * factor), (int)(5 * factor), (int)(5 * factor));
+			IArea second = new Area(1, 1, 5, 5);
+            
+			SubTile t1 = new SubTile(second, new Item(3, 3, Color.Green));
+			t1.Fill(c => new Item(c.X, c.Y, c.X + c.Y == 6 ? Color.Green : Color.Yellow));
 
-        [Test, Category("Image hash")]
-        public void Crop()
-        {
-            Tile<Item> t0 = new Tile<Item>(new Area(0, 0, 100, 100), new Item(0, 0, Color.Red));
+			MainTile t0 = new MainTile(first, t1);
+			t0.Fill(c => t1.Clone(c.X, c.Y) as SubTile);
 
-            t0.Fill(c => c.X > 25 && c.X < 75 && c.Y > 30 && c.Y < 60  ? new Item(c.X, c.Y, c.X == c.Y  ? Color.Yellow: Color.Green) : new Item(c.X, c.Y, Color.Red));
+			t0.ElementSizeX = 50f / factor * second.SizeX;
+			t0.ElementSizeY = 50f / factor * second.SizeY;
+			t0.ElementStepX = 50f / factor * second.SizeX;
+			t0.ElementStepY = 50f / factor * second.SizeY;
+			t0.RefOffsetX = 50;
+			t0.RefOffsetY = 0;
 
-            string signature0 = t0.AsQuantified().GetImage(1000, 1000,
-                (z, s) => z.ToBitmap(100, 50, z.X + "\n" + z.Y)
-                ).Item.GetSignature();
-            Assert.AreEqual("BFE39DA3858C0A979B54F99442B397DA", signature0, "Image hash");
-            Assert.AreEqual("0,0;100,100", t0.GetArea().ToString(), "Area");
+			return t0;
+		}
 
-            IEnumerable<Item> crop1 = t0.Crop(new Area(25, 30, 75, 60));
-            Tile<Item> t1 = new Tile<Item>(crop1);
+		#endregion
 
-            string signature1 = t1.AsQuantified().GetImage(1000, 1000, (z, s) =>
+		[Test, Category("Image hash")]
+		public void Crop()
+		{
+			Tile<Item> t0 = new Tile<Item>(new Area(0, 0, 100, 100), new Item(0, 0, Color.Red));
+
+			t0.Fill(c => c.X > 25 && c.X < 75 && c.Y > 30 && c.Y < 60 ? new Item(c.X, c.Y, c.X == c.Y ? Color.Yellow : Color.Green) : new Item(c.X, c.Y, Color.Red));
+
+
+
+			var q0 = t0.AsQuantified();
+
+			var c0 = t0.GetChecksum(i => i.Color.Name);
+			var s0 = q0.GetImage(1000, 1000, (z, s) => z.ToBitmap(100, 50, z.X + "\n" + z.Y)).Item.GetSignature();
+
+
+			Assert.AreEqual("BFE39DA3858C0A979B54F99442B397DA", s0, "Image hash");
+			Assert.AreEqual("0,0;100,100", t0.GetArea().ToString(), "Area");
+
+			IEnumerable<Item> crop1 = t0.Crop(new Area(25, 30, 75, 60));
+			Tile<Item> t1 = new Tile<Item>(crop1);
+
+			string signature1 = t1.AsQuantified().GetImage(1000, 1000, (z, s) =>
                 z.ToBitmap(100, 50, z.X + "\n" + z.Y)).Item.GetSignature();
 
-            Assert.AreEqual("742D809F5440028ED7F86072C4FC2FA9", signature1, "Image hash");
-            Assert.AreEqual("25,30;75,60", t1.GetArea().ToString(), "Area");
+			Assert.AreEqual("742D809F5440028ED7F86072C4FC2FA9", signature1, "Image hash");
+			Assert.AreEqual("25,30;75,60", t1.GetArea().ToString(), "Area");
 
 
-            IEnumerable<Item> crop2 = t0.Crop(t => t.Color != Color.Yellow);
-            Tile<Item> t2 = new Tile<Item>(crop2);
+			IEnumerable<Item> crop2 = t0.Crop(t => t.Color != Color.Yellow);
+			Tile<Item> t2 = new Tile<Item>(crop2);
 
-            string signature2 = t2.AsQuantified().GetImage(1000, 1000, (z, s) =>
+			string signature2 = t2.AsQuantified().GetImage(1000, 1000, (z, s) =>
                 z.ToBitmap(100, 50, z.X + "\n" + z.Y)).Item.GetSignature();
 
-            Assert.AreEqual("6A226FC4E4EC36837BA5042FBFE8D923", signature2, "Image hash");
-            Assert.AreEqual("31,31;59,59", t2.GetArea().ToString(), "Area");
+			Assert.AreEqual("6A226FC4E4EC36837BA5042FBFE8D923", signature2, "Image hash");
+			Assert.AreEqual("31,31;59,59", t2.GetArea().ToString(), "Area");
 
-        }
-    }
+		}
+	}
 }
